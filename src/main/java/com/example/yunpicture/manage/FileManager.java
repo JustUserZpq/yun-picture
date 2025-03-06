d820ff8f30d080ff4d4fcd46649faee265317d76
JustUserZpq <zhangpengqi63@gmail.com>
2025-03-06 21:48:57 +0800

    完成图片模块的开发

---
+++ src/main/java/com/example/yunpicture/manage/FileManager.java
@@ -0,0 +1,124 @@
+package com.example.yunpicture.manage;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.NumberUtil;
+import cn.hutool.core.util.RandomUtil;
+import com.example.yunpicture.config.CosClientConfig;
+import com.example.yunpicture.exception.BusinessException;
+import com.example.yunpicture.exception.ErrorCode;
+import com.example.yunpicture.exception.ThrowUtils;
+import com.example.yunpicture.model.dto.file.UploadPictureResult;
+import com.qcloud.cos.model.PutObjectResult;
+import com.qcloud.cos.model.ciModel.persistence.ImageInfo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @ClassName -> FileManager
+ * @Description
+ * @Author User
+ * @Date 2025/3/6 18:17 星期四
+ * @Version 3.3
+ */
+@Service
+@Slf4j
+public class FileManager {
+
+    @Resource
+    private CosClientConfig cosClientConfig;
+
+    @Resource
+    private CosManager cosManager;
+
+
+    /**
+     * 上传图片
+     *
+     * @param multipartFile    文件
+     * @param uploadPathPrefix 上传路径前缀
+     * @return
+     */
+    public UploadPictureResult uploadPicture(MultipartFile multipartFile, String uploadPathPrefix) {
+        // 校验图片
+        validPicture(multipartFile);
+        // 图片上传地址
+        String uuid = RandomUtil.randomString(16);
+        String originFilename = multipartFile.getOriginalFilename();
+        // 自己拼接文件上传路径，而不是原始文件名称
+        String uploadFilename = String.format("%s_%s.%s", DateUtil.formatDate(new Date()), uuid,
+                FileUtil.getSuffix(originFilename));
+        // 最终路径
+        String uploadPath = String.format("/%s/%s", uploadPathPrefix, uploadFilename);
+        File file = null;
+        try {
+            // 创建临时文件
+            file = File.createTempFile(uploadPath, null);
+            multipartFile.transferTo(file);
+            // 拿到上传图片对象
+            PutObjectResult putObjectResult = cosManager.putPictureObject(uploadPath, file);
+            ImageInfo imageInfo = putObjectResult.getCiUploadResult().getOriginalInfo().getImageInfo();
+            // 封装返回结果
+            UploadPictureResult uploadPictureResult = new UploadPictureResult();
+            int picWidth = imageInfo.getWidth();
+            int picHeight = imageInfo.getHeight();
+            //宽高比
+            double picScale = NumberUtil.round(picWidth * 1.0 / picHeight, 2).doubleValue();
+            uploadPictureResult.setPicName(FileUtil.mainName(originFilename));
+            uploadPictureResult.setPicWidth(picWidth);
+            uploadPictureResult.setPicHeight(picHeight);
+            uploadPictureResult.setPicScale(picScale);
+            uploadPictureResult.setPicFormat(imageInfo.getFormat());
+            uploadPictureResult.setPicSize(FileUtil.size(file));
+            uploadPictureResult.setUrl(cosClientConfig.getHost() + "/" + uploadPath);
+            return uploadPictureResult;
+        } catch (Exception e) {
+            log.error("图片上传到对象存储失败", e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "上传失败");
+        } finally {
+            //临时文件清理
+            this.deleteTempFile(file);
+        }
+    }
+
+    /**
+     * 校验文件
+     *
+     * @param multipartFile multipart 文件
+     */
+    public void validPicture(MultipartFile multipartFile) {
+        ThrowUtils.throwIf(multipartFile == null, ErrorCode.PARAMS_ERROR, "文件不能为空");
+        // 1. 校验文件大小
+        long fileSize = multipartFile.getSize();
+        // 1MB
+        final long ONE_M = 1024 * 1024L;
+        ThrowUtils.throwIf(fileSize > 2 * ONE_M, ErrorCode.PARAMS_ERROR, "文件大小不能超过 2M");
+        // 2. 校验文件后缀
+        String fileSuffix = FileUtil.getSuffix(multipartFile.getOriginalFilename());
+        // 允许上传的文件后缀
+        final List<String> ALLOW_FORMAT_LIST = Arrays.asList("jpeg", "jpg", "png", "webp");
+        ThrowUtils.throwIf(!ALLOW_FORMAT_LIST.contains(fileSuffix), ErrorCode.PARAMS_ERROR, "文件类型错误");
+    }
+
+    /**
+     * 删除临时文件
+     */
+    public void deleteTempFile(File file) {
+        if (file == null) {
+            return;
+        }
+        // 删除临时文件
+        boolean deleteResult = file.delete();
+        if (!deleteResult) {
+            log.error("file delete error, filepath = {}", file.getAbsolutePath());
+        }
+    }
+
+}
---
+++ src/main/java/com/example/yunpicture/mapper/PictureMapper.java
@@ -0,0 +1,18 @@
+package com.example.yunpicture.mapper;
+
+import com.example.yunpicture.model.entity.Picture;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author User
+* @description 针对表【picture(图片)】的数据库操作Mapper
+* @createDate 2025-03-06 17:50:39
+* @Entity com.example.yunpicture.model.entity.Picture
+*/
+public interface PictureMapper extends BaseMapper<Picture> {
+
+}
+
+
+
+
